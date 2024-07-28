e7157b3a0cdb97de5121f8f7a8b170881d64c6ac
Amin394 <amin394@outlook.com>
2024-07-28 20:44:56 +0100

    اعتبار المقال الحالي في الرابط لتفعيل مشاركة المقال

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,6 +7,7 @@ function Navigation({ setCurrentBlog, searchWord, setSearchWord }) {
     startTransition(() => setSearchWord(query.toLowerCase()));
     setCurrentBlog(0);
     window.scrollTo(0, 0);
+    history.pushState({}, '', window.location.pathname);
   };
   
   return (
